311aeaffee774c6608d32e29a8c58b1b0e603b50
Carlos5Noob <cchaati2101@g.educaand.es>
2025-06-18 00:05:33 +0200

    REFACTOR: diseño de la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
 import Button from './components/Button'
 import { useState } from 'react';
+import Icon from './components/Icon';
+import iconDelete from './assets/eliminar.png'
+import tarea from './assets/tarea.png'
 
 function App() {
   const [inputValue, setInputValue] = useState<string>('');
+  const [success, setSuccess] = useState<string>('');
+  const [failure, setFailure] = useState<string>('');
   const [lista, setLista] = useState<string[]>([]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,25 +16,40 @@ function App() {
   }
 
   const agregar = () => {
+    if (inputValue === '') {
+      return;
+    }
     setLista([...lista, inputValue]);
+    setInputValue('');
+    setSuccess('Tarea agregada correctamente');
+    setFailure('');
   }
 
   const eliminar = (item: string) => {
     setLista(lista.filter((i) => i !== item));
+    setFailure('Tarea eliminada correctamente');
+    setSuccess('');
   }
 
   return (
     <>
-    <div className='flex flex-col items-center justify-center bg-amber-50 p-3'>
-      <h1 className='text-6xl text-white bg-purple-950 p-6 border-8 border-teal-500 rounded-2xl'>TODO-List</h1>
+    <div className='flex flex-col items-center justify-center bg-amber-50 p-5 ml-50 rounded-2xl w-3xl'>
+      <h1 className='font-semibold  text-5xl text-black mt-5 flex gap-3'>ToDo List <span><Icon src={tarea} width={50} height={50}/></span></h1>
       <div>
-        <input value={inputValue} onChange={handleChange} className='border-2 border-teal-500 mt-20 w-5xl mr-5' type="text" placeholder='Añade algo nuevo...' />
-        <Button onClick={agregar} className='bg-green-400 p-3 border-amber-600 border-2 rounded-4xl hover: cursor-pointer active:bg-amber-600 font-bold'>Añadir</Button>
+        <input value={inputValue} onChange={handleChange} className='bg-gray-200 rounded-4xl p-3 mt-20 w-lg mr-5' type="text" placeholder='Añade algo nuevo...' />
+        <Button onClick={agregar} className='bg-green-400 p-3 border-green-800 border-2 rounded-full hover: cursor-pointer active:bg-green-300 font-bold'>Añadir</Button>
+      </div>
+      <div className='mt-5 mb-2 mr-95'>
+        <p className='text-green-600'>{success && success}</p>
+        <p className='text-red-500'>{failure && failure}</p>
       </div>
       <div>
         <ul>
           {lista.map((item, index) => (
-            <li key={index}>{item}<Button onClick={() => eliminar(item)} className='bg-red-400 ml-2 p-3 border-amber-600 border-2 rounded-4xl hover: cursor-pointer active:bg-red-800 font-bold'>Eliminar</Button></li>
+            <div className=' w-lg bg-gray-200 rounded-lg p-3 pb-1 mt-3 mr-25 mb-2 flex justify-between'>
+              <li className='max-w-1' key={index}>{item}</li>
+              <Button className='cursor-pointer' onClick={() => eliminar(item)}><Icon src={iconDelete} width={40} height={40}/></Button>
+            </div>
           ))}
         </ul> 
       </div>
