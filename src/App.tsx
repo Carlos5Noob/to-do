d86bd908d13e7b56fa476fa92e4e4fcf478edfa6
Carlos5Noob <cchaati2101@g.educaand.es>
2025-06-17 20:18:57 +0200

    Inicio proyecto. Tailwind añadido

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='items-center justify-center border-4'>
+        <h1 className='bg-amber-300 text-5xl p-7'>Hola mundo!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
